e5fb805887076e1c21e05e2a12b546b4ff2a07aa
Igor Miguel <igormiguelbs@gmail.com>
2020-09-21 12:43:54 -0300

    Quiz Page Pt. 1

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -14,6 +14,37 @@ function Quiz() {
 
       <section className="quiz">
 
+        <div className="content">
+
+          <div className="pergunta">
+
+            <h2>Pergunta 01</h2>
+
+            <p>Qual médico você deve consultar regularmente para fazer a prevenção ao câncer de próstata?</p>
+
+            <form>
+
+              <input type="radio" id="male" name="gender" value="male" />
+              <label for="male">A – oncologista</label>
+              <br/>
+              <input type="radio" id="female" name="gender" value="female" />
+              <label for="female">B – clínico geral</label>
+              <br/>
+              <input type="radio" id="other" name="gender" value="other" />
+              <label for="other">C – urologista</label>
+              <br/>
+              <input type="radio" id="other" name="gender" value="other" />
+              <label for="other">D - cardiologista</label>
+
+              <div className="clear"></div>
+
+              <button className="button border">Enviar resposta</button>
+
+            </form>
+
+            </div>
+
+        </div>
         
       </section>
 
