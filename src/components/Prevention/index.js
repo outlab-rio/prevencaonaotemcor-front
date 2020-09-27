d3c968258256a76a3142249eea11bc63fa9c296c
Igor Miguel <igormiguelbs@gmail.com>
2020-09-26 22:13:54 -0300

    Prevention Videos

--- src/components/Prevention/index.js
+++ src/components/Prevention/index.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from "react-slick";
+
+import ReactModal from 'react-modal';
 
 import './style.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
 import doctor from '../../assets/images/doctor.jpg'
+
+import videodiagnosticoprostata from '../../assets/videos/video-diagnostico-prostata.mp4'
+import videodiagnosticoprostatatwo from '../../assets/videos/video-diagnostico-prostata-2.mp4'
+import videoestatisticasprostata from '../../assets/videos/video-estatisticas-prostata.mp4'
+import videoriscoprostata from '../../assets/videos/video-fator-risco-prostata.mp4'
+import videoprevencaoprostata from '../../assets/videos/video-prevencao-prostata.mp4'
+import videoestatisticamama from '../../assets/videos/video-estatisticas-mama.mp4'
+import videoriscomama from '../../assets/videos/video-fator-risco-mama.mp4'
+import videosinaismama from '../../assets/videos/video-sinais-sintomas-mama.mp4'
 
 function PreventionSection(){
 
-  const settings = {
-    //dots: true,
-    arrows: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+  const [showModal, setshowModal] = useState(false);
+  const [showModal2, setshowModal2] = useState(false);
+  const [showModal3, setshowModal3] = useState(false);
+  const [showModal4, setshowModal4] = useState(false);
+  const [showModal5, setshowModal5] = useState(false);
+  const [showModal6, setshowModal6] = useState(false);
+  const [showModal7, setshowModal7] = useState(false);
+  const [showModal8, setshowModal8] = useState(false);
 
   const centerSlide = {
     className: "center",
@@ -30,106 +44,45 @@ function PreventionSection(){
 
     <>
 
-      {/*<section className="formasdeprevencao" id="prevention">
+      <ReactModal isOpen={showModal} className="Modal" >
+        <FontAwesomeIcon icon={faTimes} onClick={e => setshowModal(false)} />
+        <video controls><source src={videodiagnosticoprostata} type="video/mp4" /></video>
+      </ReactModal>
 
-       <div className="third left">
+      <ReactModal isOpen={showModal2} className="Modal" >
+        <FontAwesomeIcon icon={faTimes} onClick={e => setshowModal2(false)} />
+        <video controls><source src={videodiagnosticoprostatatwo} type="video/mp4" /></video>
+      </ReactModal>
 
-        <Slider {...settings}>
+      <ReactModal isOpen={showModal3} className="Modal" >
+        <FontAwesomeIcon icon={faTimes} onClick={e => setshowModal3(false)} />
+        <video controls><source src={videoestatisticasprostata} type="video/mp4" /></video>
+      </ReactModal>
 
-          <div>
+      <ReactModal isOpen={showModal4} className="Modal" >
+        <FontAwesomeIcon icon={faTimes} onClick={e => setshowModal4(false)} />
+        <video controls><source src={videoriscoprostata} type="video/mp4" /></video>
+      </ReactModal>
 
-            <h4>Consultar um urologista anualmente e realizar os exames de rotina</h4>
+      <ReactModal isOpen={showModal5} className="Modal" >
+        <FontAwesomeIcon icon={faTimes} onClick={e => setshowModal5(false)} />
+        <video controls><source src={videoprevencaoprostata} type="video/mp4" /></video>
+      </ReactModal>
 
-            <p>Aos que possuem histórico na família desse tipo de tumor, recomenda-se a visita ao urologista a partir dos 45 anos. Para isso, manter em dia a consulta com o urologista é fundamental. Incentive todos os homens com quem você convive a tornar um hábito de vida essa ação.</p>
+      <ReactModal isOpen={showModal6} className="Modal" >
+        <FontAwesomeIcon icon={faTimes} onClick={e => setshowModal6(false)} />
+        <video controls><source src={videoestatisticamama} type="video/mp4" /></video>
+      </ReactModal>
 
-          </div>
+      <ReactModal isOpen={showModal7} className="Modal" >
+        <FontAwesomeIcon icon={faTimes} onClick={e => setshowModal7(false)} />
+        <video controls><source src={videoriscomama} type="video/mp4" /></video>
+      </ReactModal>
 
-          <div>
-
-            <h4>Realizar os exames de toque retal e PSA</h4>
-
-            <p>O exame de toque retal é a principal forma de detecção precoce do câncer de próstata. Ele, junto do exame laboratorial, que mede a dosagem de PSA, é indicado aos homens com idade a partir dos 50 anos.</p>
-
-          </div>
-
-          <div>
-
-            <h4>Alimentar-se de forma saudável</h4>
-
-            <p>É importante deixar de lado os alimentos ultra processados, industrializados e bebidas açucaradas. Priorizar uma alimentação mais natural, rica em frutas, legumes, feijões e cereais integrais, é essencial. Que tal optar por esse cardápio em casa?</p>
-
-          </div>
-
-          <div>
-
-            <h4>Evitar o consumo de bebidas alcoólicas em excesso</h4>
-
-            <p>O etanol tem efeito cancerígeno sobre as células do corpo, e quando chega ao intestino, funciona como solvente, que facilita a entrada de outras substâncias, favorecendo o desenvolvimento do câncer. Por isso, quanto menor a exposição ao álcool, menor o risco. Vamos ser conscientes e evitar esse consumo?</p>
-
-          </div>
-
-        </Slider>
-
-      </div> 
-
-      <div className="third center">
-
-        <h3>Formas de<br/><strong>Prevenção</strong></h3>
-
-        <p>O tratamento ao câncer de mama e ao de próstata tem muito mais probabilidade de sucesso se as doenças forem diagnosticados precocemente.
-        <br/><strong>Faça sua parte: informe-se e compartilhe com amigos e familiares.</strong></p>
-
-      </div>
-
-      <div className="third right">
-
-        <Slider {...settings}>
-
-          <div>
-
-            <h4>Amamentar</h4>
-
-            <p>A amamentação é uma aliada à prevenção do câncer de mama. Isso acontece, pois, quando o bebê suga o leite, ocorre a esfoliação do tecido mamário, que contribui para a eliminação e renovação de possíveis células agredidas. Assim, muitas delas se autodestroem, inclusive algumas que poderiam ter lesões no material genético. Além disso, há a queda das taxas de hormônios que favorecem o não desenvolvimento do tumor. Por isso, incentive as mamães a amamentarem o maior tempo possível.</p>
-
-          </div>
-
-          <div>
-
-            <h4>Evitar o consumo de bebidas alcoólicas em excesso</h4>
-
-            <p>O etanol tem efeito cancerígeno sobre as células do corpo, e quando chega ao intestino, funciona como solvente, que facilita a entrada de outras substâncias, favorecendo o desenvolvimento do câncer. Por isso, quanto menor a exposição ao álcool, menor o risco. Vamos ser conscientes e evitar esse consumo?</p>
-
-          </div>
-
-          <div>
-
-            <h4>Evitar uso de hormônios sintéticos</h4>
-
-            <p>O uso de hormônios sintéticos, como anticoncepcionais e terapias de reposição hormonal, pode desequilibrar as taxas hormonais naturais da mulher. Dessa maneira, evitá-los é um método preventivo. </p>
-
-          </div>
-
-          <div>
-
-            <h4>Consultar um ginecologista anualmente e realizar os exames de rotina</h4>
-
-            <p>As chances de sucesso no tratamento do câncer de mama aumentam significativamente se o diagnóstico do tumor for precoce. Por isso, tenha em mente que visitar anualmente o médico ginecologista, além de manter os exames de rotina em dia, é essencial para a prevenção do câncer e outros tipos de doenças. Incentive as mulheres próximas a você a tornar essa ação uma rotina.</p>
-
-          </div>
-
-          <div>
-
-            <h4>Alimentar-se de forma saudável</h4>
-
-            <p>É importante deixar de lado os alimentos ultra processados, industrializados e bebidas açucaradas. Priorizar uma alimentação mais natural, rica em frutas, legumes, feijões e cereais integrais, é essencial. Que tal optar por esse cardápio em casa?</p>
-
-          </div>
-
-        </Slider>
-
-      </div>
-
-      </section> */}
+      <ReactModal isOpen={showModal8} className="Modal" >
+        <FontAwesomeIcon icon={faTimes} onClick={e => setshowModal8(false)} />
+        <video controls><source src={videosinaismama} type="video/mp4" /></video>
+      </ReactModal>
 
       <section className="dicas" id="tips">
 
@@ -139,22 +92,28 @@ function PreventionSection(){
 
         <Slider {...centerSlide}>
           <div>
-            <img src={doctor} alt="doctor" />
+            <img src={doctor} alt="doctor click" onClick={e => setshowModal(true)} />
           </div>
           <div>
-            <img src={doctor} alt="doctor" />
+            <img src={doctor} alt="doctor" onClick={e => setshowModal2(true)} />
           </div>
           <div>
-            <img src={doctor} alt="doctor" />
+            <img src={doctor} alt="doctor" onClick={e => setshowModal3(true)} />
           </div>
           <div>
-            <img src={doctor} alt="doctor" />
+            <img src={doctor} alt="doctor" onClick={e => setshowModal4(true)} />
           </div>
           <div>
-            <img src={doctor} alt="doctor" />
+            <img src={doctor} alt="doctor" onClick={e => setshowModal5(true)} />
           </div>
           <div>
-            <img src={doctor} alt="doctor" />
+            <img src={doctor} alt="doctor" onClick={e => setshowModal6(true)} />
+          </div>
+          <div>
+            <img src={doctor} alt="doctor" onClick={e => setshowModal7(true)} />
+          </div>
+          <div>
+            <img src={doctor} alt="doctor" onClick={e => setshowModal8(true)} />
           </div>
         </Slider>
 
